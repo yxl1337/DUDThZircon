618127912f69d846cc88041c3d09215ef099e3d5
Noah McLean <noahmc@ku.edu>
2025-03-07 14:23:48 -0600

    add second axis to log(DU) vs log(fO2) plot
    add QFM axis to top of figure

--- DvsfO2_wtdLeastSquares.m
+++ DvsfO2_wtdLeastSquares.m
@@ -64,11 +64,14 @@ fO2_highfO2 = fO2(hasAllUData & ~infO2Bounds & inDUBounds);
 %% plot U results
 
 figure("Name", "log(DU) vs fO2")
-plot(fO2_U, logDU, '.', 'MarkerSize', 15)
+t = tiledlayout(1,1);
+ax1 = axes(t);
+
+plot(ax1, fO2_U, logDU, '.', 'MarkerSize', 15)
 hold on
-line([fO2_U'; fO2_U'], [logDU' - 2*logDU_1sAbs_od'; logDU' + 2*logDU_1sAbs_od'], ...
+line(ax1, [fO2_U'; fO2_U'], [logDU' - 2*logDU_1sAbs_od'; logDU' + 2*logDU_1sAbs_od'], ...
     'Color', rgb('Barney'), 'LineWidth', 2)
-line([fO2_U'; fO2_U'], [logDU' - 2*logDU_1sAbs'; logDU' + 2*logDU_1sAbs'], ...
+line(ax1, [fO2_U'; fO2_U'], [logDU' - 2*logDU_1sAbs'; logDU' + 2*logDU_1sAbs'], ...
     'Color', rgb('Vibrant Blue'), 'LineWidth', 1.5)
 
 xbuf = 0.5; npts = 500;
@@ -82,19 +85,20 @@ for idx = 1:npts
     uncty(idx) = sqrt( positionVector(idx,:) * S * positionVector(idx,:)');
 end
 
-plot(xvector, yvector, '-', "Color", rgb('Dark Pastel Green'), ...
+plot(ax1, xvector, yvector, '-', "Color", rgb('Dark Pastel Green'), ...
     "LineWidth", 2)
-plot(xvector, yvector + 2*uncty, '-', "Color", rgb('Pine'), "LineWidth", 1.5)
-plot(xvector, yvector - 2*uncty, '-', "Color", rgb('Pine'), "LineWidth", 1.5)
+plot(ax1, xvector, yvector + 2*uncty, '-', "Color", rgb('Pine'), "LineWidth", 1.5)
+plot(ax1, xvector, yvector - 2*uncty, '-', "Color", rgb('Pine'), "LineWidth", 1.5)
 
-set(gca, "FontSize", 18)
-xlabel("log(fO2)", "FontSize", 24)
-ylabel("log(DU)", "FontSize", 24)
+set(ax1, "FontSize", 18)
+xlabel(ax1, "log({\itf}O2)", "FontSize", 24)
+ylabel(ax1, "log(DU)", "FontSize", 24)
 
 currentXLim = xlim(gca);
 newXLim(1) = max([fO2_lowerBound, currentXLim(1), xlimits(1)]);
-newXLim(2) = min([fO2_upperBound, currentXLim(2), xlimits(2)]);
-xlim(newXLim)
+newXLim(2) = min([fO2_upperBound, currentXLim(2), xlimits(2)]) + 0.2;
+xlim(ax1, newXLim)
+ax1.XTickMode = "auto";
 
 % % uncomment if plotting high-fO2 points
 % plot(fO2_highfO2, logDU_highfO2, '.b', 'MarkerSize', 15)
@@ -103,13 +107,30 @@ xlim(newXLim)
 %     'Color', rgb('black'), 'LineWidth', 2)
 % xlim([newXLim(1) 0])
 
-annotation("textbox", [0.4 0.12 0.5, 0.1], ...
-    "String", "Slope = " + round(x(1),2) + " ± " + round(2*stdx(1),2), ...
-    "FontSize", 18, ...
+annotation("textbox", [0.4 0.14 0.5, 0.1], ...
+    "String", ...
+    "Slope = " + round(x(1),2) + " ± " + round(2*stdx(1),2) + " (2\sigma)", ...
+    "FontSize", 20, ...
     "HorizontalAlignment", "center", ...
     "VerticalAlignment","middle", ...
     "LineStyle", "none")
 
+ax2 = axes(t);
+ax2.XAxisLocation = 'top';
+ax2.YAxisLocation = 'right';
+ax2.Color = 'none';
+set(ax2, "FontSize", 17)
+
+ax1xlim = xlim(ax1);
+T = 1000 + 273.15; % assume a temperature in K for calcs
+FMQ = 9 - 25738/T; % per Mike
+ax2xlim = ax1xlim - FMQ;
+xlim(ax2, ax2xlim)
+xlabel(ax2, "log({\itf}O2), \DeltaQFM", "FontSize", 23)
+ylabel(ax2, "")
+ax2.XTickMode = "auto";
+ax2.YTickLabel = [];
+ax2.YTick = [];
 
 %% Local functions
 
