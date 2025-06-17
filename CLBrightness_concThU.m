9710da358cd09f1d5f3ac30836d992492b10c572
Noah McLean <noahmc@ku.edu>
2025-06-17 12:50:52 -0500

    add mean and uncertainty to dark vs light DTh/DU

--- CLBrightness_concThU.m
+++ CLBrightness_concThU.m
@@ -36,6 +36,20 @@ unctColumns = ["ste_DU", "ste_DTh", "ste_DThDU"];
 diffs = dark(:,meanColumns) - light(:,meanColumns);
 uncts = sqrt( dark(:,unctColumns).^2 + light(:,unctColumns).^2 );
 
+% calculate mean and overdispersion for DTh/DU differences
+function L = loglik(data, uncts, MLE)
+    mu = MLE(1);
+    xi = MLE(2);
+    varxi = uncts.^2 + xi;
+    logliki = (data - mu).^2 ./ varxi + log(varxi);
+    L = sum(logliki);
+end % function loglik
+% Perform maximum likelihood estimation for the log-likelihood function
+initialMLE = [0, 1]; % Initial guesses for mu and xi
+options = optimset('Display', 'off', 'TolX', 1e-12, 'TolFun', 1e-12, 'MaxIter', 1e6, 'MaxFunEval', 1e6);
+MLE = fminunc(@(params) loglik(diffs.mean_DThDU, uncts.ste_DThDU, params), initialMLE, options);
+
+
 %% set up a plot
 
 unctlinewidth = 10;
@@ -63,15 +77,19 @@ xticklabels(dark.mixName)
 yline(0, 'LineWidth', ylinewidth)
 ylabel("DTh_{dark} - DTh_{light}", 'FontSize', 20)
 
+%%
 figure
-errorbar(diffs.mean_DThDU, 2*uncts.ste_DThDU, '_', 'LineWidth', ...
+hold on
+line([0, 13], [0, 0], 'LineWidth', ylinewidth, 'Color', 'k')
+line([0, 13], [MLE(1), MLE(1)], 'LineWidth', 2, 'Color', '#006400')
+eb = errorbar(diffs.mean_DThDU, 2*uncts.ste_DThDU, '_', 'LineWidth', ...
     unctlinewidth, 'CapSize', 0, 'MarkerSize', 8, ...
-    'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k')
+    'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k');
+eb.Color = 'b';
 set(gca, "FontSize", 16)
 xlim([0, 13])
+yregion(MLE(1)+2*sqrt(MLE(2)), MLE(1)-2*sqrt(MLE(2)), 'LineWidth', 1, 'FaceColor', '#77DD77')
 xticks(1:12)
 xticklabels(dark.mixName)
-yline(0, 'LineWidth', ylinewidth)
 ylabel("DTh/DU_{dark} - DTh/DU_{light}", 'FontSize', 20)
-set(gcf, 'InvertHardcopy', 'off');
-saveas(gcf, 'DThDU_sector_zoning.png')
+exportgraphics(gca, 'DThDU_sector_zoning_updated.png', 'BackgroundColor', 'white')
